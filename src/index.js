75cbb48ca67cdafe83324e6222f60bfd6afd5079
ntwcklng <ntwcklng@gmail.com>
2017-12-18 16:16:15 +0100

    remove pc

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ module.exports = ({ markdownAST }, options = { width: 600, height: 300 }) => {
 
       if (isUrlValid(videoUrl)) {
         node.type = `html`
-        node.value = `<iframe src="${videoUrl}" width="${options.width}px" height="${options.height}px"/>`
+        node.value = `<div><iframe src="${videoUrl}" width="${options.width}" height="${options.height}"></iframe></div>`
       }
     }
   })
