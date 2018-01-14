da656a6788407844983427698372ecaa6566a26f
Neal <info@nealbuerger.com>
2018-01-14 20:26:10 +0100

    Rewrote for Vimeo and Youtube and Pure ID support

--- src/index.js
+++ src/index.js
@@ -1,24 +1,47 @@
 'use strict'
-const visit = require(`unist-util-visit`)
+const visit = require(`unist-util-visit`);
+const getVideoId = require('get-video-id');
 
-module.exports = ({ markdownAST }, options = { width: 600, height: 300 }) => {
-  function isUrlValid(userInput) {
-    var res = userInput.match(
-      /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
-    )
-    if (res == null) return false
-    else return true
+module.exports = ({ markdownAST }, options = { width: 560, height: 315 }) => {
+  let createIframe = (url) => {
+    return `<iframe 
+              width="${options.width}" 
+              height="${options.height}" 
+              src="${url}" 
+              frameborder="0" 
+              allowfullscreen
+            ></iframe>`
+  };
+  
+  let videoTypes = {
+    'youtube': (id) => createIframe(`https://www.youtube.com/embed/${id}`),
+    'vimeo': (id) => createIframe(`https://player.vimeo.com/video/${id}`),
+    'videopress': (id) => {
+      return createIframe(`https://videopress.com/embed/${id}`) + `<script src="https://videopress.com/videopress-iframe.js"></script>`
+    }
   }
+
   visit(markdownAST, `inlineCode`, node => {
     const { value } = node
 
-    if (value.startsWith(`youtube:`)) {
-      const videoUrl = value.substr(8)
+    if (value.startsWith(`video:`) || value.startsWith(`youtube:`) || value.startsWith(`vimeo:`) || value.startsWith(`videopress:`)) {
+      const processValue = value.match(/([^:])*:(.*)/);
+      const videoId = getVideoId(processValue[2]);
 
-      if (isUrlValid(videoUrl)) {
-        node.type = `html`
-        node.value = `<div><iframe src="${videoUrl}" width="${options.width}" height="${options.height}"></iframe></div>`
+      let output;
+      if (videoId) {
+        output = videoTypes[videoId.service](videoId.id);
+      } else {
+        let service = processValue[1];
+        if (service === video) {
+          output = `<p style="color: red">Error: Video Id could not be read.</p>`
+        }
+        output = videoTypes[service](videoId);
       }
+
+      node.type = `html`
+      node.value = output;
+
     }
   })
 
